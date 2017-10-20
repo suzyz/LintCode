b0c3743f744277b46a746835d00cd729d5908f6a
suzyz <suzyzhang0@gmail.com>
2017-10-20 22:17:43 +0800

    backup on 20171020.

--- DistinctSubsequences/distinct_subsequences.cpp
+++ DistinctSubsequences/distinct_subsequences.cpp
@@ -8,6 +8,32 @@ public:
     int numDistinct(string S, string T) {
     	int n = S.length(), m = T.length();
 
-    	
+		int ans = 1;
+		for (int i = 1; i <= n; ++i)
+		{
+			int k = 0;
+			while (k<i && k<m)
+			{
+				if (!is_substring(i-k-1,k+1,S,T))
+					break;
+				++k;
+			}
+			ans += k-1;
+		}
+		return ans;
+    }
+
+    bool is_substring(int st,int len,string &s,string &t)
+    {
+    	string cur = s.substr(st,len);
+    	int idx = 0;
+    	for (int i = 0; i < t.length(); ++i)
+    	{
+    		if (s[idx] == t[i])
+    			++idx;
+    		if (idx >= len)
+    			break;
+    	}
+    	return (idx >= len);
     }
 };
